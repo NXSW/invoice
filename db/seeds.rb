4c1c4cfd96102078691b6805850726efecd38f67
王寧思 <wangningsi@ouneshis-MacBook-Pro.local>
2018-10-17 17:06:02 +0900

    seed変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Member.create(
-  [
-  	{ name: '王', Company_number: '001', sex: '男', workplace: 'NEXT', cost: 300000, upper_standerd: 180, upper_standerd: 140 },
-  	{ name: '甲田', Company_number: '002', sex: '女', workplace: 'NEXT', cost: 200000, upper_standerd: 180, upper_standerd: 140 },
-  	{ name: '前森', Company_number: '003', sex: '男', workplace: 'NEXT', cost: 200000, upper_standerd: 180, upper_standerd: 140 }
-  ]
-)
+if Member.count == 0
+  Member.create(
+    [
+    	{ name: '王', Company_number: '001', sex: '男', workplace: 'NEXT', cost: 300000, upper_standerd: 180, upper_standerd: 140 },
+    	{ name: '甲田', Company_number: '002', sex: '女', workplace: 'NEXT', cost: 200000, upper_standerd: 180, upper_standerd: 140 },
+    	{ name: '前森', Company_number: '003', sex: '男', workplace: 'NEXT', cost: 200000, upper_standerd: 180, upper_standerd: 140 }
+    ]
+  )
+end
