48c32a9f8985a31e6d0f1e76dd964873c431fb49
王寧思 <wns19830308@gmail.com>
2018-10-26 12:27:35 +0900

    seedファイル編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,18 +7,18 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 if Member.count == 0
-  Member.create!(
-    [
-    	{ name: '王', sex: 0, work_place: '日立', cost: 300000, upper_standerd: 180, lower_standerd: 140, bill_to_id: 1 },
-    	{ name: '甲田', sex: 0, work_place: 'NEXT', cost: 20000, upper_standerd: 180, lower_standerd: 140, bill_to_id: 2 },
-    	{ name: '前森', sex: 0, work_place: 'NEXT', cost: 20000, upper_standerd: 180, lower_standerd: 140, bill_to_id: 3 }
-    ]
-  )
   BillTo.create!(
     [
       { name: 'Zero' },
       { name: 'レバテック' },
       { name: 'レバテック' }
+    ]
+  )
+  Member.create!(
+    [
+    	{ name: '王', sex: 0, work_place: '日立', cost: 300000, upper_standerd: 180, lower_standerd: 140, bill_to_id: 1 },
+    	{ name: '甲田', sex: 0, work_place: 'NEXT', cost: 20000, upper_standerd: 180, lower_standerd: 140, bill_to_id: 2 },
+    	{ name: '前森', sex: 0, work_place: 'NEXT', cost: 20000, upper_standerd: 180, lower_standerd: 140, bill_to_id: 3 }
     ]
   )
 end
