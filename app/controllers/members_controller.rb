f2438eb5f973be07f03d551505db52459364969d
kodakaoru <kaoru.koda@nxsw.co.jp>
2018-10-31 17:53:16 +0900

    社員情報のデザイン変更

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -6,7 +6,7 @@ class MembersController < ApplicationController
   def index
     @q = Member.ransack(params[:q])
     @members = @q.result(distinct: true)
-    @members = Member.page(params[:page]).per(2)
+    @members = Member.page(params[:page]).per(50)
   end
 
   # GET /members/1
