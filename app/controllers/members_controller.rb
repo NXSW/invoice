377d76d99b97f7d01752157eac082be77b75676f
naoyamaemori <naoya.maemori@nxsw.co.jp>
2018-11-01 16:38:06 +0900

    ストップ

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -6,8 +6,8 @@ class MembersController < ApplicationController
   def index
     @q = Member.ransack(params[:q])
     @members = @q.result(distinct: true)
-    @members = Member.page(params[:page]).per(50)
-    
+    @members = @q.result(distinct: true)
+    @bill_tos = BillTo.all
   end
 
   # GET /members/1
